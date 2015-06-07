b2167daaf0e7226e0e4d8e07023b29c4a75f7968
Micah Hawkins <micahbhawkins@gmail.com>
2015-06-07 09:49:15 -0700

    Download derivative for specific item

--- derivatives.js
+++ derivatives.js
@@ -1,18 +1,20 @@
 var fs = require('fs'),
     http = require('http'),
     url = 'http://Medusa.local:8090/api/v1',
-    path = '/catalog/CB2AFCFB-0262-51AB-C327-927AA2F79D9C/asset/554/_derivative?',
+    assetId = 548,
+    path = '/catalog/CB2AFCFB-0262-51AB-C327-927AA2F79D9C/asset/'+assetId+'/_derivative?',
     session = 'session=TOKEN-be62606a-b201-4a98-a1ce-2fdc492d9ed4',
     async = 'async=false',
-    imageOptions = 'imageOptions={
-                        "outputFileFormat": "JPEG",
-                        "colormode": "RGB",
-                        "resizeWidth": 512,
-                        "resizeHeight": 600,
-                        "compressionQuality": 95,
-                        "source": "original",
-                        "resolution": 72,
-                        "compressionType": "JPEG" }',
+    imageOptions = 'imageOptions={'+
+                       '"outputFileFormat": "JPEG",'+
+                       '"colormode": "RGB",'+
+                       '"resizeWidth": 56,'+
+                       '"resizeHeight": 50,'+
+                       '"compressionQuality": 95,'+
+                       '"source": "original",'+
+                       '"resolution": 50,'+
+                       '"compressionType": "JPEG"'+
+                   '}',
     delim = '&',
     filename = 'model.jpg';
 
@@ -24,17 +26,9 @@ http.get(url+
     delim+
     imageOptions,
     function(res) {
-        var body = '';
-
-        res.setEncoding('utf8');
-        res.on('data', function(chunk) {
-            body += chunk;
-        });
-        res.on('end', function() {
-            try {
-                catList = JSON.parse(body);
-            } catch(er) {
-                console.log(er);
-            }
+        res.pipe(fs.createWriteStream(filename)).on('close', function() {
+            console.log('done');
+        }).on('close', function() {
+            console.log('double done');
         });
     });
