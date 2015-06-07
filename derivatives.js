df823b0bc479d9c76e783ddc664c8c29bc2963e0
Micah Hawkins <micahbhawkins@gmail.com>
2015-06-06 23:20:07 -0700

    moving to another machine

--- derivatives.js
+++ derivatives.js
@@ -1,7 +1,28 @@
-var http = require('http');
-var catList;
+var fs = require('fs'),
+    http = require('http'),
+    url = 'http://Medusa.local:8090/api/v1',
+    path = '/catalog/CB2AFCFB-0262-51AB-C327-927AA2F79D9C/asset/554/_derivative?',
+    session = 'session=TOKEN-be62606a-b201-4a98-a1ce-2fdc492d9ed4',
+    async = 'async=false',
+    imageOptions = 'imageOptions={
+                        "outputFileFormat": "JPEG",
+                        "colormode": "RGB",
+                        "resizeWidth": 512,
+                        "resizeHeight": 600,
+                        "compressionQuality": 95,
+                        "source": "original",
+                        "resolution": 72,
+                        "compressionType": "JPEG" }',
+    delim = '&',
+    filename = 'model.jpg';
 
-http.get('http://Medusa.local:8090/api/v1/catalog/?session=TOKEN-be62606a-b201-4a98-a1ce-2fdc492d9ed4',
+http.get(url+
+    path+
+    session+
+    delim+
+    async+
+    delim+
+    imageOptions,
     function(res) {
         var body = '';
 
@@ -17,4 +38,3 @@ http.get('http://Medusa.local:8090/api/v1/catalog/?session=TOKEN-be62606a-b201-4
             }
         });
     });
-
