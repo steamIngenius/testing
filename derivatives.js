05a38cc5f0921365874dd223a371c26f3e1be8c6
Micah Hawkins <micahbhawkins@gmail.com>
2015-06-08 11:43:04 -0700

    Clean up construction of imageOptions

--- derivatives.js
+++ derivatives.js
@@ -1,28 +1,31 @@
 var fs = require('fs'),
     http = require('http'),
-    url = 'http://Medusa.local:8090/api/v1',
-    assetId = 537,
-    path = '/catalog/CB2AFCFB-0262-51AB-C327-927AA2F79D9C/asset/'+assetId+'/_derivative?',
-    session = 'session=TOKEN-be62606a-b201-4a98-a1ce-2fdc492d9ed4',
+    url = 'http://Modok:8090/api/v1',
+    assetId = 334,
+    path = '/catalog/4903AB2A-16E2-9444-9B03-DFE0EA598C7B/asset/'+assetId+'/_derivative?',
+    session = 'session=TOKEN-fb2db149-6b45-47e3-a651-8d074345c6eb',
     async = 'async=false',
-    imageOptions = 'imageOptions={'+
-                       '"outputFileFormat": "JPEG",'+
-                       '"colormode": "RGB",'+
-                       '"resizeWidth": 700,'+
-                       '"resizeHeight": 1050,'+
-                       '"compressionQuality": 95,'+
-                       '"source": "original",'+
-                       '"resolution": 72,'+
-                       '"compressionType": "JPEG"'+
-                   '}',
+    imageOptions = 'imageOptions='+JSON.stringify({
+        "outputFileFormat": "JPEG",
+        "colormode": "RGB",
+        "resizeWidth": 700,
+        "resizeHeight": 1050,
+        "compressionQuality": 95,
+        "source": "original",
+        "resolution": 72,
+        "compressionType": "JPEG"
+    }),
     and = '&',
     filename = 'model.jpg';
 
+console.log(imageOptions);
+
 http.get(url+path+session+and+async+and+imageOptions,
     function(res) {
-        res.pipe(fs.createWriteStream(filename)).on('close', function() {
-            console.log('done');
+        res.pipe(fs.createWriteStream(filename)).on('error', function(err) {
+            console.log(err);
+            res.read();
         }).on('close', function() {
-            console.log('double done');
+            console.log('done');
         });
     });
