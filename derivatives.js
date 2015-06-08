42ac7ab362e14e20ebe2369515fcae6ca63c19b2
Micah Hawkins <micahbhawkins@gmail.com>
2015-06-07 21:30:21 -0700

    Clean up get string

--- derivatives.js
+++ derivatives.js
@@ -1,30 +1,24 @@
 var fs = require('fs'),
     http = require('http'),
     url = 'http://Medusa.local:8090/api/v1',
-    assetId = 548,
+    assetId = 537,
     path = '/catalog/CB2AFCFB-0262-51AB-C327-927AA2F79D9C/asset/'+assetId+'/_derivative?',
     session = 'session=TOKEN-be62606a-b201-4a98-a1ce-2fdc492d9ed4',
     async = 'async=false',
     imageOptions = 'imageOptions={'+
                        '"outputFileFormat": "JPEG",'+
                        '"colormode": "RGB",'+
-                       '"resizeWidth": 56,'+
-                       '"resizeHeight": 50,'+
+                       '"resizeWidth": 700,'+
+                       '"resizeHeight": 1050,'+
                        '"compressionQuality": 95,'+
                        '"source": "original",'+
-                       '"resolution": 50,'+
+                       '"resolution": 72,'+
                        '"compressionType": "JPEG"'+
                    '}',
-    delim = '&',
+    and = '&',
     filename = 'model.jpg';
 
-http.get(url+
-    path+
-    session+
-    delim+
-    async+
-    delim+
-    imageOptions,
+http.get(url+path+session+and+async+and+imageOptions,
     function(res) {
         res.pipe(fs.createWriteStream(filename)).on('close', function() {
             console.log('done');
